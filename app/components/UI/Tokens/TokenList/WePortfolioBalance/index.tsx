dc60ac5bc92a08fc38759420a2a0713fce358ca9
ylshie <yuliang.hsieh@gmail.com>
2025-04-30 13:27:48 +0800

    add WePortfolioBalance

---
+++ app/components/UI/Tokens/TokenList/WePortfolioBalance/index.tsx
@@ -0,0 +1,200 @@
+import React, { useCallback } from 'react';
+import { View, TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { useSelector } from 'react-redux';
+import { useMetrics } from '../../../../hooks/useMetrics';
+import { useTheme } from '../../../../../util/theme';
+import AppConstants from '../../../../../core/AppConstants';
+import Engine from '../../../../../core/Engine';
+import Routes from '../../../../../constants/navigation/Routes';
+import { MetaMetricsEvents } from '../../../../../core/Analytics';
+import { selectPrivacyMode } from '../../../../../selectors/preferencesController';
+import { RootState } from '../../../../../reducers';
+import { isPortfolioUrl } from '../../../../../util/url';
+import createStyles from '../../styles';
+import Button, {
+  ButtonVariants,
+  ButtonSize,
+  ButtonWidthTypes,
+} from '../../../../../component-library/components/Buttons/Button';
+import Text, { TextVariant } from '../../../../../component-library/components/Texts/Text';
+import SensitiveText, {
+  SensitiveTextLength,
+} from '../../../../../component-library/components/Texts/SensitiveText';
+import Icon, {
+  IconSize,
+  IconName,
+} from '../../../../../component-library/components/Icons/Icon';
+import { BrowserTab } from '../../types';
+import { WalletViewSelectorsIDs } from '../../../../../../e2e/selectors/wallet/WalletView.selectors';
+import { strings } from '../../../../../../locales/i18n';
+import { EYE_SLASH_ICON_TEST_ID, EYE_ICON_TEST_ID } from './index.constants';
+import AggregatedPercentageCrossChains from '../../../../../component-library/components-temp/Price/WeAggregatedPercentage/AggregatedPercentageCrossChains';
+import { useMultichainBalances } from '../../../../hooks/useMultichainBalances';
+import Loader from '../../../../../component-library/components-temp/Loader/Loader';
+
+export const PortfolioBalance = React.memo(() => {
+  const { PreferencesController } = Engine.context;
+  const { colors } = useTheme();
+  const styles = createStyles(colors);
+  const browserTabs = useSelector((state: RootState) => state.browser.tabs);
+  const privacyMode = useSelector(selectPrivacyMode);
+  const isMultichainBalancesCollectionForMarketingEnabled = useSelector(
+    (state: RootState) => state.security.dataCollectionForMarketing,
+  );
+  const navigation = useNavigation();
+  const { trackEvent, isEnabled, createEventBuilder } = useMetrics();
+
+  const { selectedAccountMultichainBalance } = useMultichainBalances();
+
+  const onOpenPortfolio = useCallback(() => {
+    const existingPortfolioTab = browserTabs.find(({ url }: BrowserTab) =>
+      isPortfolioUrl(url),
+    );
+
+    let existingTabId;
+    let newTabUrl;
+    if (existingPortfolioTab) {
+      existingTabId = existingPortfolioTab.id;
+    } else {
+      const analyticsEnabled = isEnabled();
+      const portfolioUrl = new URL(AppConstants.PORTFOLIO.URL);
+
+      portfolioUrl.searchParams.append('metamaskEntry', 'mobile');
+
+      // Append user's privacy preferences for metrics + marketing on user navigation to Portfolio.
+      portfolioUrl.searchParams.append(
+        'metricsEnabled',
+        String(analyticsEnabled),
+      );
+      portfolioUrl.searchParams.append(
+        'marketingEnabled',
+        String(!!isMultichainBalancesCollectionForMarketingEnabled),
+      );
+
+      newTabUrl = portfolioUrl.href;
+    }
+    const params = {
+      ...(newTabUrl && { newTabUrl }),
+      ...(existingTabId && { existingTabId, newTabUrl: undefined }),
+      timestamp: Date.now(),
+    };
+    navigation.navigate(Routes.BROWSER.HOME, {
+      screen: Routes.BROWSER.VIEW,
+      params,
+    });
+    trackEvent(
+      createEventBuilder(MetaMetricsEvents.PORTFOLIO_LINK_CLICKED)
+        .addProperties({
+          portfolioUrl: AppConstants.PORTFOLIO.URL,
+        })
+        .build(),
+    );
+  }, [
+    navigation,
+    trackEvent,
+    createEventBuilder,
+    isEnabled,
+    isMultichainBalancesCollectionForMarketingEnabled,
+    browserTabs,
+  ]);
+
+  const renderAggregatedPercentage = () => {
+    if (
+      !selectedAccountMultichainBalance ||
+      !selectedAccountMultichainBalance?.shouldShowAggregatedPercentage ||
+      selectedAccountMultichainBalance?.totalFiatBalance === undefined
+    ) {
+      return null;
+    }
+
+    return (
+      <AggregatedPercentageCrossChains
+        privacyMode={privacyMode}
+        totalFiatCrossChains={selectedAccountMultichainBalance.totalFiatBalance}
+        tokenFiatBalancesCrossChains={
+          selectedAccountMultichainBalance.tokenFiatBalancesCrossChains
+        }
+      />
+    );
+  };
+
+  const toggleIsBalanceAndAssetsHidden = useCallback(
+    (value: boolean) => {
+      PreferencesController.setPrivacyMode(value);
+    },
+    [PreferencesController],
+  );
+
+  return (
+    <View style={{
+        backgroundColor: '#D7E3FC',
+        width: '100%',
+        paddingTop: 8,
+        paddingBottom: 8,
+        paddingLeft: 16,
+        //...styles.portfolioBalance
+        flexDirection: 'row',
+        alignItems: 'center',
+        //marginHorizontal: 16,
+        justifyContent: 'space-between',
+        //paddingTop: 24,
+      }}>
+      <View>
+        {/*<View>"可用餘額"</View>*/}
+        <Text>{'可用餘額'}</Text>
+        <View>
+          {selectedAccountMultichainBalance?.displayBalance ? (
+            <View style={styles.balanceContainer}>
+              <SensitiveText
+                isHidden={privacyMode}
+                length={SensitiveTextLength.Long}
+                testID={WalletViewSelectorsIDs.TOTAL_BALANCE_TEXT}
+                variant={TextVariant.DisplayMD}
+              >
+                {selectedAccountMultichainBalance.displayBalance}
+              </SensitiveText>
+              {/*
+              <TouchableOpacity
+                onPress={() => toggleIsBalanceAndAssetsHidden(!privacyMode)}
+                testID="balance-container"
+              >
+                <Icon
+                  style={styles.privacyIcon}
+                  name={privacyMode ? IconName.EyeSlash : IconName.Eye}
+                  size={IconSize.Md}
+                  color={colors.text.muted}
+                  testID={
+                    privacyMode ? EYE_SLASH_ICON_TEST_ID : EYE_ICON_TEST_ID
+                  }
+                />
+              </TouchableOpacity>
+              */}
+              {renderAggregatedPercentage()}
+            </View>
+          ) : (
+            <View style={styles.loaderWrapper}>
+              <Loader />
+            </View>
+          )}
+
+          {/*renderAggregatedPercentage()*/}
+        </View>
+      </View>
+      {/*
+      <View style={styles.portfolioButtonContainer}>
+        <Button
+          variant={ButtonVariants.Secondary}
+          size={ButtonSize.Md}
+          width={ButtonWidthTypes.Full}
+          style={styles.buyButton}
+          onPress={onOpenPortfolio}
+          label={strings('asset_overview.portfolio_button')}
+          testID={WalletViewSelectorsIDs.PORTFOLIO_BUTTON}
+          endIconName={IconName.Export}
+        />
+      </View>
+      */}
+    </View>
+  );
+});

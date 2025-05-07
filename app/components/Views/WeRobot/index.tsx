4db21f3f75c348ca92526215e716a4557bcde09b
ylshie <yuliang.hsieh@gmail.com>
2025-05-07 16:54:23 +0800

    add WeRobot

---
+++ app/components/Views/WeRobot/index.tsx
@@ -0,0 +1,571 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
+import React, {
+  useEffect,
+  useRef,
+  useCallback,
+  useContext,
+} from 'react';
+import {
+  ActivityIndicator,
+  View,
+  Linking,
+  ImageSourcePropType,
+} from 'react-native';
+
+import { connect, useSelector } from 'react-redux';
+import { baseStyles } from '../../../styles/common';
+import { getWalletNavbarOptions } from '../../UI/Navbar';
+import { strings } from '../../../../locales/i18n';
+import {
+  isPastPrivacyPolicyDate,
+  shouldShowNewPrivacyToastSelector,
+  storePrivacyPolicyShownDate as storePrivacyPolicyShownDateAction,
+  storePrivacyPolicyClickedOrClosed as storePrivacyPolicyClickedOrClosedAction,
+} from '../../../reducers/legalNotices';
+import { CONSENSYS_PRIVACY_POLICY } from '../../../constants/urls';
+import {
+  ToastContext,
+  ToastVariants,
+} from '../../../component-library/components/Toast';
+import { AvatarAccountType } from '../../../component-library/components/Avatars/Avatar';
+import NotificationsService from '../../../util/notifications/services/NotificationService';
+import Engine from '../../../core/Engine';
+import { MetaMetricsEvents } from '../../../core/Analytics';
+import OnboardingWizard from '../../UI/OnboardingWizard';
+import ErrorBoundary from '../ErrorBoundary';
+import { useTheme } from '../../../util/theme';
+import Routes from '../../../constants/navigation/Routes';
+import {
+  getDecimalChainId,
+} from '../../../util/networks';
+import {
+  selectChainId,
+  selectEvmNetworkConfigurationsByChainId,
+  selectNetworkConfigurations,
+  selectEvmTicker,
+} from '../../../selectors/networkController';
+import {
+  selectNetworkName,
+  selectNetworkImageSource,
+} from '../../../selectors/networkInfos';
+import {
+  selectTransformedTokens,
+} from '../../../selectors/tokensController';
+import {
+  NavigationProp,
+  ParamListBase,
+  useNavigation,
+} from '@react-navigation/native';
+import BannerAlert from '../../../component-library/components/Banners/Banner/variants/BannerAlert/BannerAlert';
+import { BannerAlertSeverity } from '../../../component-library/components/Banners/Banner';
+import Text, {
+  TextColor,
+} from '../../../component-library/components/Texts/Text';
+import { useMetrics } from '../../hooks/useMetrics';
+import { RootState } from '../../../reducers';
+import { selectSelectedInternalAccount } from '../../../selectors/accountsController';
+import {
+  hideNftFetchingLoadingIndicator as hideNftFetchingLoadingIndicatorAction,
+  showNftFetchingLoadingIndicator as showNftFetchingLoadingIndicatorAction,
+} from '../../../reducers/collectibles';
+import { WalletViewSelectorsIDs } from '../../../../e2e/selectors/wallet/WalletView.selectors';
+import {
+  getMetamaskNotificationsUnreadCount,
+  getMetamaskNotificationsReadCount,
+  selectIsMetamaskNotificationsEnabled,
+} from '../../../selectors/notifications';
+import { selectIsProfileSyncingEnabled } from '../../../selectors/identity';
+import { ButtonVariants } from '../../../component-library/components/Buttons/Button';
+import { useAccountName } from '../../hooks/useAccountName';
+
+import useCheckNftAutoDetectionModal from '../../hooks/useCheckNftAutoDetectionModal';
+import useCheckMultiRpcModal from '../../hooks/useCheckMultiRpcModal';
+import {
+  selectNativeEvmAsset,
+  selectStakedEvmAsset,
+} from '../../../selectors/multichain';
+/*----- [Arthur] -----*/
+/*--------------------*/
+import { Image } from 'react-native';
+import { Dimensions } from 'react-native';
+import createStyles from './styles';
+/*--------------------*/
+
+interface WalletProps {
+  navigation: NavigationProp<ParamListBase>;
+  storePrivacyPolicyShownDate: () => void;
+  shouldShowNewPrivacyToast: boolean;
+  currentRouteName: string;
+  storePrivacyPolicyClickedOrClosed: () => void;
+  showNftFetchingLoadingIndicator: () => void;
+  hideNftFetchingLoadingIndicator: () => void;
+}
+
+/**
+ * Main view for the wallet
+ */
+const MyPersona = ({
+  navigation,
+  storePrivacyPolicyShownDate,
+  shouldShowNewPrivacyToast,
+  storePrivacyPolicyClickedOrClosed,
+}: WalletProps) => {
+  const { navigate } = useNavigation();
+  const walletRef = useRef(null);
+  const theme = useTheme();
+  const { toastRef } = useContext(ToastContext);
+  const { trackEvent, createEventBuilder } = useMetrics();
+  const styles = createStyles(theme);
+  const { colors } = theme;
+
+  const networkConfigurations = useSelector(selectNetworkConfigurations);
+  const evmNetworkConfigurations = useSelector(
+    selectEvmNetworkConfigurationsByChainId,
+  );
+
+  const selectedInternalAccount = useSelector(selectSelectedInternalAccount);
+  /**
+   * An array that represents the user tokens by chainId and address
+   */
+  const tokensByChainIdAndAddress = useSelector(selectTransformedTokens);
+  /**
+   * Current provider ticker
+   */
+  const ticker = useSelector(selectEvmTicker);
+  /**
+   * Current onboarding wizard step
+   */
+  // TODO: Replace "any" with type
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const wizardStep = useSelector((state: any) => state.wizard.step);
+  const chainId = useSelector(selectChainId);
+
+  const isDataCollectionForMarketingEnabled = useSelector(
+    (state: RootState) => state.security.dataCollectionForMarketing,
+  );
+  /**
+   * Is basic functionality enabled
+   */
+  const basicFunctionalityEnabled = useSelector(
+    (state: RootState) => state.settings.basicFunctionalityEnabled,
+  );
+
+  const { isEnabled: getParticipationInMetaMetrics } = useMetrics();
+
+  const isParticipatingInMetaMetrics = getParticipationInMetaMetrics();
+
+  const currentToast = toastRef?.current;
+
+  const accountName = useAccountName();
+
+  const accountAvatarType = useSelector((state: RootState) =>
+    state.settings.useBlockieIcon
+      ? AvatarAccountType.Blockies
+      : AvatarAccountType.JazzIcon,
+  );
+
+  useEffect(() => {
+    if (
+      isDataCollectionForMarketingEnabled === null &&
+      isParticipatingInMetaMetrics &&
+      isPastPrivacyPolicyDate
+    ) {
+      navigate(Routes.MODAL.ROOT_MODAL_FLOW, {
+        screen: Routes.SHEET.EXPERIENCE_ENHANCER,
+      });
+    }
+  }, [
+    isDataCollectionForMarketingEnabled,
+    isParticipatingInMetaMetrics,
+    navigate,
+  ]);
+
+  useEffect(() => {
+    if (!shouldShowNewPrivacyToast) return;
+
+    storePrivacyPolicyShownDate();
+    currentToast?.showToast({
+      variant: ToastVariants.Plain,
+      labelOptions: [
+        {
+          label: strings(`privacy_policy.toast_message`),
+          isBold: false,
+        },
+      ],
+      closeButtonOptions: {
+        label: strings(`privacy_policy.toast_action_button`),
+        variant: ButtonVariants.Primary,
+        onPress: () => {
+          storePrivacyPolicyClickedOrClosed();
+          currentToast?.closeToast();
+        },
+      },
+      linkButtonOptions: {
+        label: strings(`privacy_policy.toast_read_more`),
+        onPress: () => {
+          storePrivacyPolicyClickedOrClosed();
+          currentToast?.closeToast();
+          Linking.openURL(CONSENSYS_PRIVACY_POLICY);
+        },
+      },
+      hasNoTimeout: true,
+    });
+  }, [
+    storePrivacyPolicyShownDate,
+    shouldShowNewPrivacyToast,
+    storePrivacyPolicyClickedOrClosed,
+    currentToast,
+  ]);
+
+  const isNotificationEnabled = useSelector(
+    selectIsMetamaskNotificationsEnabled,
+  );
+
+  const isProfileSyncingEnabled = useSelector(selectIsProfileSyncingEnabled);
+
+  const unreadNotificationCount = useSelector(
+    getMetamaskNotificationsUnreadCount,
+  );
+
+  const readNotificationCount = useSelector(getMetamaskNotificationsReadCount);
+  const name = useSelector(selectNetworkName);
+  
+  const networkName = networkConfigurations?.[chainId]?.name ?? name;
+
+  const networkImageSource = useSelector(selectNetworkImageSource);
+  
+  const nativeEvmAsset = useSelector(selectNativeEvmAsset);
+  const stakedEvmAsset = useSelector(selectStakedEvmAsset);
+
+  /**
+   * Shows Nft auto detect modal if the user is on mainnet, never saw the modal and have nft detection off
+   */
+  useCheckNftAutoDetectionModal();
+
+  /**
+   * Show multi rpc modal if there are networks duplicated and if never showed before
+   */
+  useCheckMultiRpcModal();
+
+  /**
+   * Callback to trigger when pressing the navigation title.
+   */
+  const onTitlePress = useCallback(() => {
+    navigate(Routes.MODAL.ROOT_MODAL_FLOW, {
+      screen: Routes.SHEET.NETWORK_SELECTOR,
+    });
+    trackEvent(
+      createEventBuilder(MetaMetricsEvents.NETWORK_SELECTOR_PRESSED)
+        .addProperties({
+          chain_id: getDecimalChainId(chainId),
+        })
+        .build(),
+    );
+  }, [navigate, chainId, trackEvent, createEventBuilder]);
+
+  /**
+   * Check to see if notifications are enabled
+   */
+  useEffect(() => {
+    async function checkIfNotificationsAreEnabled() {
+      await NotificationsService.isDeviceNotificationEnabled();
+    }
+    checkIfNotificationsAreEnabled();
+  });
+
+  useEffect(
+    () => {
+      requestAnimationFrame(async () => {
+        const { AccountTrackerController } = Engine.context;
+
+        Object.values(evmNetworkConfigurations).forEach(
+          ({ defaultRpcEndpointIndex, rpcEndpoints }) => {
+            AccountTrackerController.refresh(
+              rpcEndpoints[defaultRpcEndpointIndex].networkClientId,
+            );
+          },
+        );
+      });
+    },
+    /* eslint-disable-next-line */
+    // TODO: The need of usage of this chainId as a dependency is not clear, we shouldn't need to refresh the native balances when the chainId changes. Since the pooling is always working in the back. Check with assets team.
+    // TODO: [SOLANA] Check if this logic supports non evm networks before shipping Solana
+    [navigation, chainId, evmNetworkConfigurations],
+  );
+
+  useEffect(() => {
+    if (!selectedInternalAccount) return;
+    navigation.setOptions(
+      getWalletNavbarOptions(
+        walletRef,
+        selectedInternalAccount,
+        accountName,
+        accountAvatarType,
+        networkName,
+        networkImageSource,
+        onTitlePress,
+        navigation,
+        colors,
+        isNotificationEnabled,
+        isProfileSyncingEnabled,
+        unreadNotificationCount,
+        readNotificationCount,
+        '自助客服機器人'
+      ),
+    );
+  }, [
+    selectedInternalAccount,
+    accountName,
+    accountAvatarType,
+    navigation,
+    colors,
+    networkName,
+    networkImageSource,
+    onTitlePress,
+    isNotificationEnabled,
+    isProfileSyncingEnabled,
+    unreadNotificationCount,
+    readNotificationCount,
+  ]);
+
+  const turnOnBasicFunctionality = useCallback(() => {
+    navigation.navigate(Routes.MODAL.ROOT_MODAL_FLOW, {
+      screen: Routes.SHEET.BASIC_FUNCTIONALITY,
+    });
+  }, [navigation]);
+
+  const win = Dimensions.get('window');
+  const onPressGeneral = () => {
+    trackEvent(createEventBuilder(MetaMetricsEvents.SETTINGS_GENERAL).build());
+    navigation.navigate('SecurityPersona');
+  };
+  
+  const RenderItem = ({icon, caption}: {icon: string, caption: string}) => (
+              <View style={{
+                width: '100%',
+                flexDirection: 'column',
+                marginBottom: 10,
+              //...styles.debug,
+              }}>
+                <Text style={{
+                  fontSize: 14,
+                  fontWeight: '600',
+                  color: '#272729',
+                }}>{icon}</Text>
+                <Text style={{
+                  fontSize: 12,
+                  fontWeight: '400',
+                  color: '#8C8D99',
+                }}>{caption}</Text>
+              </View>
+  )
+  const styleArticle = {
+    color: '#0066F1',
+    fontSize: 16,
+    fontWeight: '400',
+  }
+  const renderContent = useCallback(() => {
+    const assets = tokensByChainIdAndAddress
+      ? [...tokensByChainIdAndAddress]
+      : [];
+    if (nativeEvmAsset) {
+      assets.push(nativeEvmAsset);
+    }
+    if (stakedEvmAsset) {
+      assets.push(stakedEvmAsset);
+    }
+    return (
+      <View
+        style={{
+          alignItems: 'center',
+          ...styles.wrapper
+        }}
+        testID={WalletViewSelectorsIDs.WALLET_CONTAINER}
+      >
+        {!basicFunctionalityEnabled ? (
+          <View style={styles.banner}>
+            <BannerAlert
+              severity={BannerAlertSeverity.Error}
+              title={strings('wallet.banner.title')}
+              description={
+                <Text color={TextColor.Info} onPress={turnOnBasicFunctionality}>
+                  {strings('wallet.banner.link')}
+                </Text>
+              }
+            />
+          </View>
+        ) : null}
+        <>
+          <View style={{
+            width: '90%',
+          //margin: 10,
+          marginTop: 30,
+          }}>
+            <View style={{
+              width: '100%',
+              height: 75,
+              backgroundColor: '#DAEAFF',
+              borderTopLeftRadius: 30,
+              borderTopRightRadius: 30,
+            //...styles.debug,
+            }}>
+            </View>
+            <View style={{
+              width: '100%',
+              height: 50,
+              flexDirection: 'row',
+              justifyContent: 'center',
+              alignItems: 'center',
+              backgroundColor: '#678DFF'
+            }}>
+              <Text style={{
+                width: '100%',
+                color: '#FFFFFF',
+                fontSize: 14,
+                fontWeight: '400',
+                textAlign: 'center',
+              }}>
+                WeZan Customer Service Bot
+              </Text>
+            </View>
+            <View style={{
+              backgroundColor: '#DAEAFF',
+              width: '100%',
+              height: 200,
+              marginTop: 30,
+              justifyContent: 'flex-end',
+            }}>
+              <Text>請點擊問題標籤，客服進一步了了解您的需求</Text>
+              <View style={{
+                position: 'relative',
+                width: '90%',
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                margin: 10,
+              }}>
+                <View style={{
+                      backgroundColor: '#3281EC',
+                      borderRadius: 5,
+                }}>
+                  <Text>提款問題</Text>
+                </View>
+                <View style={{
+                      backgroundColor: '#3281EC',
+                      borderRadius: 5,
+                }}>
+                  <Text>帳號驗證失敗</Text>
+                </View>
+                <View style={{
+                      backgroundColor: '#3281EC',
+                      borderRadius: 5,
+                }}>
+                  <Text>帳號風控</Text>
+                </View>
+              </View>
+              <View style={{
+                position: 'relative',
+                width: '90%',
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                margin: 10,
+              }}>
+                <View style={{
+                      backgroundColor: '#3281EC',
+                      borderRadius: 5,
+                }}>
+                  <Text>刷卡問題</Text>
+                </View>
+                <View style={{
+                      backgroundColor: '#3281EC',
+                      borderRadius: 5,
+                }}>
+                  <Text>內部轉帳問題</Text>
+                </View>
+                <View style={{
+                      backgroundColor: '#3281EC',
+                      borderRadius: 5,
+                }}>
+                  <Text>提款問題</Text>
+                </View>
+              </View>
+              <View style={{
+                      backgroundColor: '#003DF5',
+                      borderRadius: 10,
+                      margin: 10,
+              }}>
+                <Text style={{
+                  color: '#FFFFFF',
+                  width: '100%',
+                  textAlign: 'center',
+                }}>Start chat</Text>
+              </View>
+            </View>     
+          </View>       
+        </>
+      </View>
+    );
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [
+    styles,
+    colors,
+    basicFunctionalityEnabled,
+    turnOnBasicFunctionality,
+    navigation,
+    ticker,
+    tokensByChainIdAndAddress,
+  ]);
+  const renderLoader = useCallback(
+    () => (
+      <View style={styles.loader}>
+        <ActivityIndicator size="small" />
+      </View>
+    ),
+    [styles],
+  );
+
+  /**
+   * Return current step of onboarding wizard if not step 5 nor 0
+   */
+  const renderOnboardingWizard = useCallback(
+    () =>
+      [1, 2, 3, 4, 5, 6, 7].includes(wizardStep) && (
+        <OnboardingWizard
+          navigation={navigation}
+          coachmarkRef={walletRef.current}
+        />
+      ),
+    [navigation, wizardStep],
+  );
+
+  return (
+    <ErrorBoundary navigation={navigation} view="MyWallet">
+      <View style={baseStyles.flexGrow}>
+        {selectedInternalAccount ? renderContent() : renderLoader()}
+
+        {renderOnboardingWizard()}
+      </View>
+    </ErrorBoundary>
+  );
+};
+
+// TODO: Replace "any" with type
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const mapStateToProps = (state: any) => ({
+  shouldShowNewPrivacyToast: shouldShowNewPrivacyToastSelector(state),
+  receiveAsset: state.modals.receiveAsset,  // [Arthur] {connect PaymentRequest}
+});
+
+// TODO: Replace "any" with type
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const mapDispatchToProps = (dispatch: any) => ({
+  storePrivacyPolicyShownDate: () =>
+    dispatch(storePrivacyPolicyShownDateAction(Date.now())),
+  storePrivacyPolicyClickedOrClosed: () =>
+    dispatch(storePrivacyPolicyClickedOrClosedAction()),
+  showNftFetchingLoadingIndicator: () =>
+    dispatch(showNftFetchingLoadingIndicatorAction()),
+  hideNftFetchingLoadingIndicator: () =>
+    dispatch(hideNftFetchingLoadingIndicatorAction()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(MyPersona);

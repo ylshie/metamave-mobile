f101a8bcba5af2a94b9348902710885330148441
ylshie <yuliang.hsieh@gmail.com>
2025-06-10 13:33:56 +0800

    refine member layout

--- app/components/Views/WeMember/index.tsx
+++ app/components/Views/WeMember/index.tsx
@@ -4,6 +4,7 @@ import React, {
   useRef,
   useCallback,
   useContext,
+  ReactChildren,
 } from 'react';
 import {
   ActivityIndicator,
@@ -11,6 +12,7 @@ import {
   Linking,
   TextInput,
   ImageSourcePropType,
+  ScrollView,
 } from 'react-native';
 
 import { connect, useSelector } from 'react-redux';
@@ -349,6 +351,130 @@ const MySocial = ({
                 }}>{caption}</Text>
               </View>
   )
+  const RenderTitle = ({title}: {title: string}) => {
+    return  <View style={{
+                      flexDirection: 'row',
+                      justifyContent: 'flex-start',
+                  }}>
+              <Circle name='circle' width={32} height={32}/>
+              <View style={{
+                backgroundColor: '#F78106',
+                borderRadius: 20,
+                paddingTop: 5,
+                paddingBottom: 5,
+                paddingRight: 30,
+                paddingLeft: 30,
+                marginLeft: 10,
+              }}>
+                <Text style={{
+                  color: '#FFFFFF',
+                  fontSize: 16,
+                  fontWeight: '400',
+                //backgroundColor: 'red',
+                }}>{title}</Text>
+              </View>
+            </View>
+  }
+  const RenderLevel = ({level}:{level: number}) => {
+    return  <Text style={{
+                  color: '#000000',
+                  fontSize: 52,
+                  lineHeight: 60,
+                  fontWeight: '800',
+                }}
+            >
+              LV{level}
+            </Text>
+  }
+  const RenderCondition = ({cond}:{cond: string}) => {
+    return <>
+              <View style={{
+                marginBottom: 5,
+              }}>
+                <Text style={{
+                      color: '#828282',
+                      fontSize: 18,
+                      fontWeight: '600',
+                    }}>
+                升級條件
+              </Text>
+              </View>
+              <Text style={{
+                      color: '#000000',
+                      fontSize: 18,
+                      fontWeight: '600',
+                    }}>
+                {cond}
+              </Text>
+    </>
+  }
+  const RenderMerit = ({text}: {text:string}) => {
+    return  <View style={{
+              flexDirection: 'row',
+            }}>
+              <Done name='done' width={24} height={24}/>
+              <Text style={{
+                color: '#000000',
+                fontSize: 18,
+                fontWeight: '500',
+              }}>{text}</Text>
+            </View>
+  }
+  const RenderMember = ({title, level, cond, children}:
+                        { 
+                          title: string, 
+                          level: number,
+                          cond: string,
+                          children: React.ReactNode | string
+                        }) => {
+  
+    return  <View style={{
+                width: '100%',
+                backgroundColor: '#FFFFFF',
+                borderWidth: 1, 
+                borderStyle: 'solid', 
+                borderColor: '#000000',
+                borderRadius: 25,
+                padding: 25,
+              }}
+            >
+              <View style={{
+                marginBottom: 20,
+              }}>
+                <RenderTitle title={title}/>
+              </View>
+              <View style={{
+                marginBottom: 20,
+              }}>
+                <RenderLevel level={level}/>
+              </View>
+              <View style={{
+                marginBottom: 20,
+              }}>
+                <RenderCondition cond={cond}/>
+              </View>
+              <View style={{
+                marginTop: 20,
+                marginBottom: 10,
+              }}>
+                <Text style={{
+                  color: '#000000',
+                  fontSize: 18,
+                  fontWeight: '700',
+                }}>
+                權益
+                </Text>
+                {children}
+              </View>
+          </View>
+  }
+  const Item = ({text}: {text:string}) => 
+    <Text style={{
+      fontSize: 15,
+      fontWeight: '500',
+    }}>
+      {text}
+    </Text>
   const renderContent = useCallback(() => {
     const assets = tokensByChainIdAndAddress
       ? [...tokensByChainIdAndAddress]
@@ -368,95 +494,71 @@ const MySocial = ({
         }}
         testID={WalletViewSelectorsIDs.WALLET_CONTAINER}
       >
-        <>
-          <View style={{
-                  width: '100%',
-                  backgroundColor: '#FFFFFF',
-                  borderWidth: 1, 
-                  borderStyle: 'solid', 
-                  borderColor: '#000000',
-                  borderRadius: 25,
-                  padding: 25,
-                }}
-          >
-            <View style={{
-              flexDirection: 'row',
-              justifyContent: 'flex-start',
-            }}>
-              <Circle name='circle' width={32} height={32}/>
-              <View style={{
-                backgroundColor: '#F78106',
-                borderRadius: 20,
-                paddingTop: 5,
-                paddingBottom: 5,
-                paddingRight: 30,
-                paddingLeft: 30,
-                marginLeft: 10,
-              }}>
-                <Text style={{
-                  color: '#FFFFFF',
-                  fontSize: 16,
-                  fontWeight: '400',
-                //backgroundColor: 'red',
-                }}>普通會員</Text>
-              </View>
-            </View>
-            <Text style={{
-              color: '#000000',
-              fontSize: 52,
-              lineHeight: 60,
-              fontWeight: '800',
-            }}>LV0</Text>
-            <Text style={{
-              color: '#828282',
-              fontSize: 18,
-              fontWeight: '600',
-            }}>升級條件</Text>
-            <Text style={{
-              color: '#000000',
-              fontSize: 18,
-              fontWeight: '600',
-            }}>免費註冊</Text>
-
-            <View>
-              <Text style={{
-                color: '#000000',
-                fontSize: 18,
-                fontWeight: '700',
-              }}>權益</Text>
-              <View style={{
-                flexDirection: 'row',
-              }}>
-                <Done name='done' width={24} height={24}/>
-                <Text style={{
-                  color: '#000000',
-                  fontSize: 18,
-                  fontWeight: '500',
-                }}>積分抽獎</Text>
-              </View>
-              <View style={{
-                flexDirection: 'row',
-              }}>
-                <Done name='done' width={24} height={24}/>
-                <Text style={{
-                  color: '#000000',
-                  fontSize: 18,
-                  fontWeight: '500',
-                }}>積分換禮品</Text>
-              </View>
-              <View style={{
-                flexDirection: 'row',
-              }}>
-                <Done name='done' width={24} height={24}/>
-                <Text style={{
-                  color: '#000000',
-                  fontSize: 18,
-                  fontWeight: '500',
-                }}>基本遊戲功能</Text>
-              </View>
-            </View>
+        <ScrollView
+          style={{
+            width: '100%',
+          }}
+          contentContainerStyle={{
+          //justifyContent: 'center',
+          //alignItems: 'center',
+          //paddingTop: 60,
+          //paddingBottom: 70,
+          }}
+        >
+          <View style={{marginTop: 20}}>
+            <RenderMember 
+              title='普通會員' 
+              level={0}
+              cond='免費註冊'
+            >
+              <RenderMerit text='積分抽獎'/>
+              <RenderMerit text='積分換禮品'/>
+              <RenderMerit text='基本遊戲功能'/>
+            </RenderMember>
           </View>
-        </>
+          <View style={{marginTop: 20}}>
+            <RenderMember 
+                title='進階會員' 
+                level={1}
+                cond='近30天累計消費積分 ≥ 10,000'
+            >
+              <RenderMerit text='積分抽獎'/>
+              <RenderMerit text='積分換禮品'/>
+              <RenderMerit text='抽獎額外 +1 次每日免費機會'/>
+              <RenderMerit text='特殊禮品兌換資格'/>
+              <RenderMerit text='基本遊戲功能'/>
+            </RenderMember>
+          </View>
+          <View style={{marginTop: 20}}>
+            <RenderMember 
+                title='VIP會員' 
+                level={2}
+                cond='近30天累計消費積分 ≥ 50,000'
+            >
+              <RenderMerit text='積分抽獎'/>
+              <RenderMerit text='積分換禮品'/>
+              <RenderMerit text='每筆積分消費額外 3% 積分回饋'/>
+              <RenderMerit text='專屬 VIP 活動邀請'/>
+              <RenderMerit text='抽獎額外 +2 次每日免費機會'/>
+            </RenderMember>
+          </View>
+          <View style={{
+                  padding: 10,
+                  marginTop: 20
+                }
+          }>
+            <Text style={{
+              fontSize: 18,
+            }}>「消費積分」說明:</Text>
+            <Text>&nbsp;</Text>
+            <Item text='為用戶於平台內發生的積分消耗行為，包括：'/>
+            <Item text='1. U卡消費所獲得的積分（刷 U卡 → 獲積分 → 用積分）'/>
+            <Item text='2. 平台內參加抽獎、遊戲下注、兌換禮品、直播刷禮物等積分支出'/>
+            <Item text='可設定不同權重：'/>
+            <Item text='- U卡消費 1U = 2 消費積分（雙倍計算）'/>
+            <Item text='- 平台內積分支出 1 積分 = 1 消費積分'/>
+          </View>
+        </ScrollView>
       </View>
     );
     // eslint-disable-next-line react-hooks/exhaustive-deps

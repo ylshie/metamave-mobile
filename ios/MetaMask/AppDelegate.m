c37d4569130a7cdcf83808a071a3aaf605cef7d6
ylshie <yuliang.hsieh@gmail.com>
2025-08-21 16:06:54 +0800

    remove Flipper

--- ios/MetaMask/AppDelegate.m
+++ ios/MetaMask/AppDelegate.m
@@ -7,6 +7,7 @@
 #import <RNBranch/RNBranch.h>
 #if DEBUG
 #ifdef FB_SONARKIT_ENABLED
+/*
 #import <FlipperKit/FlipperClient.h>
 #import <FlipperKitLayoutPlugin/FlipperKitLayoutPlugin.h>
 #import <FlipperKitLayoutPlugin/SKDescriptorMapper.h>
@@ -14,6 +15,7 @@
 #import <FlipperKitReactPlugin/FlipperKitReactPlugin.h>
 #import <FlipperKitUserDefaultsPlugin/FKUserDefaultsPlugin.h>
 #import <SKIOSNetworkPlugin/SKIOSNetworkAdapter.h>
+*/
 #endif
 #endif
 
@@ -74,8 +76,11 @@
 }
 
 - (void) initializeFlipper:(UIApplication *)application {
+  #pragma message("===== I AM PRAGMA ======")
   #if DEBUG
+  #pragma message("===== I AM DEBUG ======")
   #ifdef FB_SONARKIT_ENABLED
+  /*
     FlipperClient *client = [FlipperClient sharedClient];
     SKDescriptorMapper *layoutDescriptorMapper = [[SKDescriptorMapper alloc] initWithDefaults];
     [client addPlugin: [[FlipperKitLayoutPlugin alloc] initWithRootNode: application withDescriptorMapper: layoutDescriptorMapper]];
@@ -83,6 +88,7 @@
     [client addPlugin: [FlipperKitReactPlugin new]];
     [client addPlugin: [[FlipperKitNetworkPlugin alloc] initWithNetworkAdapter:[SKIOSNetworkAdapter new]]];
     [client start];
+  */
   #endif
   #endif
 }
